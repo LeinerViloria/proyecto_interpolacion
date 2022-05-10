bb98fbd0bad779d7a046ab61b17bf25c2a73ca88
LeinerViloria <vilorialeiner2002@gmail.com>
2022-05-09 22:53:21 -0500

    Se agrego la parte cuadratica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocumentationComponent } from './doc/documentation/documentation.component';
+import { MainComponent } from './interpolacion/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:MainComponent
+  },
+  {
+    path:"home",
+    component:MainComponent
+  },
+  {
+    path:"documentation",
+    component:DocumentationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { MainComponent } from './interpolacion/main/main.component';
+import { DocumentationComponent } from './doc/documentation/documentation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    DocumentationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
